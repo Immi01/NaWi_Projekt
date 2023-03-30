24ebf25ed43273a64507904b4898e0b025868c5a
Noel Riedmann <noel.riedmann@student.htldornbirn.at>
2023-03-30 08:35:22 +0200

    back to basic code after input fields aint working

--- src/at/htldornbirn/projects/nawi/Team9/Main.java
+++ src/at/htldornbirn/projects/nawi/Team9/Main.java
@@ -1,39 +1,35 @@
 package at.htldornbirn.projects.nawi.Team9;
-
 import org.newdawn.slick.*;
 
 public class Main extends BasicGame {
-    //Add actors
-    private Horse horse;
-
-    //Constructor
     public Main(String title) {
         super(title);
     }
 
-    //Main method - executable
-    public static void main(String[] argv) {
+    @Override
+    public void init(GameContainer gameContainer) throws SlickException {
+
+    }
+
+    @Override
+    public void update(GameContainer gameContainer, int delta) throws SlickException {
+
+        }
+
+
+
+
+    @Override
+    public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
+
+    }
         try {
             AppGameContainer container = new AppGameContainer(new Main("Horsepower"));
-            container.setDisplayMode(1600, 900, false);
+            container.setDisplayMode(1600, 800, false);
             container.start();
         } catch (SlickException e) {
             e.printStackTrace();
         }
     }
-
-    @Override
-    public void init(GameContainer gameContainer) throws SlickException {
-        this.horse = new Horse(100, 100, 1);
-    }
-
-    @Override
-    public void update(GameContainer gameContainer, int delta) throws SlickException {
-        this.horse.update(delta);
-    }
-
-    @Override
-    public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
-        this.horse.render(graphics);
-    }
 }
+

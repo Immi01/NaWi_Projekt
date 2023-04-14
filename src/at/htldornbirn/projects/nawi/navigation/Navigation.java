3e5833ee7c8f0b2550c14b003cc037f218354c5b
JonasN05 <jonas.nigg@student.htldornbirn.at>
2023-04-14 14:21:58 +0200

    navigation del

--- src/at/htldornbirn/projects/nawi/navigation/Navigation.java
+++ src/at/htldornbirn/projects/nawi/navigation/Navigation.java
@@ -18,28 +18,22 @@ public class Navigation extends BasicGameState {
     public int getID() {
         return Constants.NAVIGATION;
     }
-    private List<Actor> actors;
+
 
     @Override
     public void init(GameContainer gameContainer, StateBasedGame stateBasedGame) throws SlickException {
-        this.actors = new ArrayList<>();
-        Team1 team1 = new Team1(100,100);
-        actors.add(team1);
+
     }
 
     @Override
     public void render(GameContainer gameContainer, StateBasedGame stateBasedGame, Graphics graphics) throws SlickException {
-        for (Actor actor: actors) {
-            actor.render(graphics);
-        }
+
 
     }
 
     @Override
     public void update(GameContainer gameContainer, StateBasedGame stateBasedGame, int delta) throws SlickException {
-        for (Actor actor : actors) {
-            actor.update(delta);
-        }
+
 
         if (gameContainer.getInput().isMousePressed(Input.MOUSE_LEFT_BUTTON)){
             System.out.println("OUT");

0acb20ad190666cce73f007838a4eb5ef4649243
Jonas Kronabitter <jonas.kronabitter@student.htldornbirn.at>
2023-04-28 14:50:30 +0200

    anfang

---
+++ src/at/htldornbirn/projects/nawi/Team4/Substanzensimulator/Substanzensimulator.java
@@ -0,0 +1,30 @@
+package at.htldornbirn.projects.nawi.Team4.Substanzensimulator;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class Substanzensimulator extends BasicGameState {
+
+    @Override
+    public int getID() {
+        return 0;
+    }
+
+    @Override
+    public void init(GameContainer gameContainer, StateBasedGame stateBasedGame) throws SlickException {
+
+    }
+
+    @Override
+    public void render(GameContainer gameContainer, StateBasedGame stateBasedGame, Graphics graphics) throws SlickException {
+
+    }
+
+    @Override
+    public void update(GameContainer gameContainer, StateBasedGame stateBasedGame, int delta) throws SlickException {
+
+    }
+}

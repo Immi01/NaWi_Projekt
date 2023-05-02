8d94d28f11971cb1773212e3a45cd0b497ed4d4a
Timothe Goldmann <timothe.goldmann@student.htldonrbirn.at>
2023-05-02 11:30:24 +0200

    fertig

--- src/at/htldornbirn/projects/nawi/Team3/StopingDistance.java
+++ src/at/htldornbirn/projects/nawi/Team3/StopingDistance.java
@@ -1,6 +1,5 @@
 package at.htldornbirn.projects.nawi.Team3;
 
-import at.htldornbirn.projects.nawi.Constants;
 import org.newdawn.slick.*;
 import org.newdawn.slick.font.effects.ColorEffect;
 import org.newdawn.slick.gui.TextField;
@@ -10,38 +9,64 @@ import org.newdawn.slick.state.StateBasedGame;
 
 public class StopingDistance extends BasicGameState {
 
-    private TextField speedField;
-    private TextField reactionTimeField;
-    private TextField decelerationField;
-    private float speed = 0;
-    private float reactionTime = 0;
-    private float deceleration = 0;
-    private float brakingDistance = 0;
-    private UnicodeFont font;
+    private Input input;
+    private float speed;
+    private float reactionTime;
+    private float deceleration;
+    private float brakingDistance;
+    private Font font;
 
 
     @Override
     public void init(GameContainer gameContainer, StateBasedGame stateBasedGame) throws SlickException {
-        // Erstellen der Textfelder für die Benutzereingabe
-        font = new UnicodeFont("arial.ttf", 10, false, false);
-        font.addAsciiGlyphs();
-        font.getEffects().add(new ColorEffect(java.awt.Color.WHITE));
-        speedField = new TextField(gameContainer, font, 200, 100, 200, 25);
-        reactionTimeField = new TextField(gameContainer, font, 200, 150, 200, 25);
-        decelerationField = new TextField(gameContainer, font, 200, 200, 200, 25);
-        speedField.setAcceptingInput(true);
-        reactionTimeField.setAcceptingInput(true);
-        decelerationField.setAcceptingInput(true);
+        input = gameContainer.getInput();
+        speed = 0;
+        reactionTime = 1f;
+        deceleration = 9.81f * 0.8f;
+        brakingDistance = 0;
+        font = new TrueTypeFont(new java.awt.Font("Verdana", java.awt.Font.BOLD, 15), true);
     }
 
     @Override
     public void render(GameContainer gameContainer, StateBasedGame stateBasedGame, Graphics g) throws SlickException {
+        // Geschwindigkeit und Bremsweg ausgeben
+        g.setFont(font);
+        g.drawString("Geschwindigkeit: " + (int) speed + " km/h", 50, 50);
+        g.drawString("Bremsweg: " + (int) brakingDistance + " m", 50, 100);
+
+        // Anleitung ausgeben
+        g.drawString("Drücke die Pfeiltasten nach oben und unten, um die Geschwindigkeit um 1 Km/h zu ändern.", 50, 520);
+        g.drawString("Drücke die Pfeiltasten nach rechts und links, um die Geschwindigkeit zu ändern.", 50, 550);
+        g.drawString("Drücke die R-Taste, um die Geschwindigkeit auf 0 zu setzen.", 50, 580);
+
 
     }
 
     @Override
     public void update(GameContainer gameContainer, StateBasedGame stateBasedGame, int delta) throws SlickException {
-// Berechnung des Anhaltewegs
+        // Benutzereingabe abfragen
+        if (input.isKeyDown(Input.KEY_UP)) {
+            speed += 5 * delta / 1000.0f;
+        }
+        if (input.isKeyDown(Input.KEY_DOWN)) {
+            speed -= 5 * delta / 1000.0f;
+        }
+        if (input.isKeyDown(Input.KEY_R)) {
+            speed = 0;
+        }
+        if (input.isKeyDown(Input.KEY_RIGHT)) {
+            speed += 50 * delta / 1000.0f;
+        }
+        if (input.isKeyDown(Input.KEY_LEFT)) {
+            speed -= 50 * delta / 1000.0f;
+        }
+
+        //Benutzereingabe überprüfen
+        if (this.speed < 0) {
+            this.speed = 0;
+        }
+
+        // Berechnung des Anhaltewegs
         float dt = delta / 1000.0f; // Zeit in Sekunden
         float reactionDistance = speed / 3.6f * reactionTime; // Reaktionsweg in Metern
         float brakingSpeed = speed / 3.6f - deceleration * reactionTime; // Geschwindigkeit beim Bremsen in m/s

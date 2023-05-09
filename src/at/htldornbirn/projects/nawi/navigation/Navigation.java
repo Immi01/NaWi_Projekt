7b355fb71ad0e693a84605b706b20d10e95f7864
Mihai <mihai.muscalu@student.htldornbirn.at>
2023-05-09 10:41:27 +0200

    after merge 1

--- src/at/htldornbirn/projects/nawi/navigation/Navigation.java
+++ src/at/htldornbirn/projects/nawi/navigation/Navigation.java
@@ -1,38 +1,36 @@
 package at.htldornbirn.projects.nawi.navigation;
 
 import at.htldornbirn.projects.nawi.Constants;
-<<<<<<< HEAD
+import at.htldornbirn.projects.nawi.tools.slider.Slider;
+import at.htldornbirn.projects.nawi.tools.slider.SliderListener;
 import org.lwjgl.input.Mouse;
 import org.newdawn.slick.*;
-=======
 import net.java.games.input.Component;
 import org.lwjgl.input.Mouse;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
->>>>>>> feature/Team2
+
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Navigation extends BasicGameState {
+public class Navigation extends BasicGameState implements SliderListener {
     @Override
     public int getID() {
         return Constants.NAVIGATION;
     }
 
-<<<<<<< HEAD
     private List<Actor> actors;
+    private Slider slider;
     float mouseX;
     float mouseY;
     Input input;
     float team1X;
     float team1Y;
-=======
->>>>>>> feature/Team2
 
     @Override
     public void init(GameContainer gameContainer, StateBasedGame stateBasedGame) throws SlickException {
@@ -42,43 +40,50 @@ public class Navigation extends BasicGameState {
         actors.add(team1);
         team1X = actors.get(0).getX();
         team1Y = actors.get(0).getY();
+        this.slider = new Slider(300,300,0,100);
+        this.slider.addListener(this);
+
 
     }
 
     @Override
     public void render(GameContainer gameContainer, StateBasedGame stateBasedGame, Graphics graphics) throws SlickException {
-<<<<<<< HEAD
+
         for (Actor actor : actors) {
             actor.render(graphics);
         }
-
-
-=======
->>>>>>> feature/Team2
+        this.slider.render(gameContainer,graphics);
 
 
     }
 
     @Override
     public void update(GameContainer gameContainer, StateBasedGame stateBasedGame, int delta) throws SlickException {
-<<<<<<< HEAD
+
         for (Actor actor : actors) {
             actor.update(delta);
         }
+        this.slider.update(gameContainer);
         mouseX = Mouse.getX();
-        mouseY = (Mouse.getY()-600)*-1;
+        mouseY = (Mouse.getY() - 600) * -1;
 
         if (input.isMousePressed(Input.MOUSE_LEFT_BUTTON)) {
-            if((mouseX >= team1X && mouseX < team1X+60) && (mouseY >= team1Y && mouseY < team1Y+60)) {
+            if ((mouseX >= team1X && mouseX < team1X + 60) && (mouseY >= team1Y && mouseY < team1Y + 60)) {
                 stateBasedGame.enterState(Constants.SINUS_FUNKTION_GAME);
             }
-=======
 
 
-        if (gameContainer.getInput().isMousePressed(Input.MOUSE_LEFT_BUTTON)){
-            System.out.println("OUT");
-            stateBasedGame.enterState(Constants.SINUS_FUNKTION_GAME);
->>>>>>> feature/Team2
+            if (gameContainer.getInput().isMousePressed(Input.MOUSE_LEFT_BUTTON)) {
+                System.out.println("OUT");
+                stateBasedGame.enterState(Constants.SINUS_FUNKTION_GAME);
+
+            }
         }
     }
+
+    @Override
+    public void onChange(float mouseY) {
+        System.out.println(mouseY);
+    }
 }
+

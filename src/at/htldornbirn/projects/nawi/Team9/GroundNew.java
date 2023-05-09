3aa59136fa12cc093e2cc7334942b863a839f5dc
santiago <santiago.scheffknecht@student.htldornbirn.at>
2023-05-09 11:35:18 +0200

    Angefangen mit neuem Groundsend nudes

---
+++ src/at/htldornbirn/projects/nawi/Team9/GroundNew.java
@@ -0,0 +1,15 @@
+package at.htldornbirn.projects.nawi.Team9;
+
+import org.newdawn.slick.Graphics;
+
+public class GroundNew implements Actor{
+    @Override
+    public void render(Graphics graphics) {
+
+    }
+
+    @Override
+    public void update(int delta) {
+
+    }
+}

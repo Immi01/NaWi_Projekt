51274a44c9fc00f7bee9f8beece8b896738f7604
Mihai <mihai.muscalu@student.htldornbirn.at>
2023-05-25 09:09:20 +0200

    FIx

--- src/at/htldornbirn/projects/nawi/Constants.java
+++ src/at/htldornbirn/projects/nawi/Constants.java
@@ -2,7 +2,7 @@ package at.htldornbirn.projects.nawi;
 
 public class Constants {
     public static final int NAVIGATION = 0;
-    public static final int  SINUS_FUNKTION_GAME = 2;
+    public static final int  TEAM2 = 2;
     public static final int  TEAM3 = 3;
     public static final int  TEAM4 = 4;
     public static final int  TEAM6 = 6;

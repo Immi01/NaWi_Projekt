935a4184e1c3ab80c2b5557701b85b6045e1a611
Mihai <mihai.muscalu@student.htldornbirn.at>
2023-05-26 13:35:07 +0200

    Team8 ID

--- src/at/htldornbirn/projects/nawi/Constants.java
+++ src/at/htldornbirn/projects/nawi/Constants.java
@@ -7,5 +7,6 @@ public class Constants {
     public static final int  TEAM4 = 4;
     public static final int  TEAM6 = 6;
     public static final int  TEAM7 = 7;
+    public static final int  TEAM8 = 8;
     public static final int  TEAM9 = 9;
 }
